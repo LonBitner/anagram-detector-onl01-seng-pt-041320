9f0bf14b8be39cffef315932b84abeba64c7e211
Lon Bitner <lonbitner@gmail.com>
2020-05-07 01:18:48 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,4 +5,10 @@ class Anagram
     @anagram = anagram
   end
   
+  def match(anagram_array)
+    anagram_array.select do |anagram|
+      anagram.split("").sort == @anagram.split("").sort
+    end
+  end
+  
 end
